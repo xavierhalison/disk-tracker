f187be1841ee1e1f93f889294e5950bf28ce13e0
Halison Xavier <halisonpx@gmail.com>
2026-09-22 11:19:09 -0300

    removing nextjs frontend

--- client/src/app/auth/actions.ts
+++
@@ -1,9 +0,0 @@
-import { loginUser } from "../../services/auth";
-
-export async function login(formData: FormData) {
-  "use server";
-  const email = formData.get("email") + "";
-  const password = formData.get("password") + "";
-
-  await loginUser(email, password);
-}
--- client/src/app/auth/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import AuthForm from "../../../components/AuthForm";
-
-export default function RegisterPage() {
-  return <AuthForm mode="register" />;
-}
